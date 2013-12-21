97ca2a5892d23140c2db16079e51d92798b42733
Scott Reilly <scott@coffee2code.com>
2013-12-21 00:42:26 -0700

    Finish rename of primary plugin file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../linkify-post-ids.php';
+	require dirname( __FILE__ ) . '/../linkify-posts.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
