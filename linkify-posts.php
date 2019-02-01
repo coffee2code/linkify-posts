36d5efd73182aedfabd403b860b0124d81af95a8
Scott Reilly <scott@coffee2code.com>
2019-01-31 21:44:25 -0700

    Update License URI to be HTTPS

--- linkify-posts.php
+++ linkify-posts.php
@@ -7,7 +7,7 @@
  * Author URI:  http://coffee2code.com/
  * Text Domain: linkify-posts
  * License:     GPLv2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Description: Turn a string, list, or array of post IDs and/or slugs into a list of links to those posts. Includes widget and template tag.
  *
  * Compatible with WordPress 3.3 through 4.7+.
