e5cde29afa5697efad138dd5e09d0c31dc07e8bd
Scott Reilly <scott@coffee2code.com>
2023-08-22 16:27:18 -0600

    Unit tests: Allow tests to run against current versions of WordPress
    * New: Add `composer.json` for PHPUnit Polyfill dependency
    * Change: Prevent PHP warnings due to missing core-related generated files

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -5,7 +5,19 @@
  * @package Linkify_Posts
  */
 
-define( 'LINKIFY_POSTS_PLUGIN_FILE', dirname( __FILE__, 3 ) . '/linkify-posts.php' );
+define( 'LINKIFY_POSTS_PLUGIN_DIR',  dirname( __FILE__, 3 ) );
+define( 'LINKIFY_POSTS_PLUGIN_FILE', LINKIFY_POSTS_PLUGIN_DIR . '/linkify-posts.php' );
+
+$polyfill_path = LINKIFY_POSTS_PLUGIN_DIR . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+if ( file_exists( $polyfill_path ) ) {
+	require $polyfill_path;
+} else {
+	echo "Error: PHPUnit Polyfills dependency not found.\n";
+	echo "Run: composer require --dev yoast/phpunit-polyfills:\"^2.0\"\n";
+	exit;
+}
+
+! defined( 'WP_RUN_CORE_TESTS' ) && define( 'WP_RUN_CORE_TESTS', false );
 
 ini_set( 'display_errors', 'on' );
 error_reporting( E_ALL );
